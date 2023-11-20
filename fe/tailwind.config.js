15980977fda4291f095e765d946606950b2cc4c6
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2023-11-20 16:01:42 +0700

    Update: read more in details

---
+++ fe/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/components/**/*.jsx",
+    "./src/pages/**/*.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
+
